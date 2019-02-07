b23ff4b1f9272de9c89a196b5deafeb2903a79e2
harald.carlsten <haraldcarlsten@yahoo.se>
2019-02-07 15:06:54 +0100

    Adding Anagram-function

---
+++ harryC72/week-04/day-4/anagram/anagram.ts
@@ -0,0 +1,41 @@
+'use strict'
+
+let alphabet: any = [
+  'a','b','c','d', 'e','f' ,'g' ,'h' ,'i' ,'j' ,
+   'k' ,'l' ,'m' ,'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t' ,'u',
+   'v' ,'w','x' ,'y' ,'z' 
+];
+
+let isAnagram = (input1:string, input2:string):boolean=>{
+
+  let tempArray: string[] = input1.split('');
+  let tempArray2: string[] = input2.split('');
+
+  if(!(input1.length === input2.length)){
+    return false;
+  }else if(checkContainSameLetters(tempArray, tempArray2)){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+let checkContainSameLetters =
+ (inputArray: string[], inputArray2: string[]):boolean =>{
+ let counter1: number = 0;
+ let counter2: number = 0;
+
+ inputArray.forEach((char, i) => {
+   if(char == alphabet[i]){ counter1 += i};
+  });
+  inputArray2.forEach((char, i) => {
+    if(char == alphabet[i]){ counter2 += i};
+   });
+
+   return (counter1 == counter2)?true : false;
+
+}
+
+console.log(isAnagram('are', 'ear'));
+console.log(isAnagram('revolver', 'car'));
+

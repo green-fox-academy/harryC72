993a4507dbb6a9a45b31ddcf467f1204968efde5
HarryXenon <haraldcarlsten@yahoo.se>
2019-03-07 14:34:41 +0100

    first working

--- harryC72/week-07/day-4/bookstore/server.js
+++ harryC72/week-07/day-4/bookstore/server.js
@@ -7,6 +7,11 @@ const PORT = 3000;
 const mysql = require('mysql');
 app.use(express.json());
 
+app.set('view engine', 'ejs');
+
+app.use('/static', express.static('static'));
+
+
 const con = mysql.createConnection({
   host: process.env.DB_host,
   user: process.env.DB_user,
@@ -14,20 +19,24 @@ const con = mysql.createConnection({
   database: process.env.DB_database
 });
 
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
+
 let SQL = `select aut_name, book_name 
 from author 
 inner join book_mast
-on aut_id`;
+on author.aut_id = book_mast.aut_id`;
 
 app.get('/booklist', (req,res) => {
-  conn.query(SQL, (err, rows) => {
+  con.query(SQL, (err, rows) => {
     if (err) {
       console.error(err);
       res.status(500).send();
       return;
     }
-    res.send(rows);
+    res.render('index', {data: rows});
     console.log(rows);
+
   });
 });
-
